24b1b621465ca13d02c244cd1404f2aa54053124
shimanyi <vshimanyi@gmail.com>
2015-06-17 16:26:26 +0300

    added CI and structured the site for dev

---
+++ app/application/controllers/relay.php
@@ -0,0 +1,27 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Relay extends CI_Controller {
+	private $data;
+
+	function __construct(){
+		parent::__construct();
+		$this->load->library('Cphpmailer');
+
+		$this->load->model('app_model');
+	}
+
+	public function _load_view(){
+		$this->load->view('inc/tmp',$this->data);
+	}
+
+	public function index(){
+		$this->home();
+	}
+
+	public function home(){
+		$this->data['title'] 	= "Home Relay";
+		$this->data['content'] 	= "home";
+
+		$this->_load_view();
+	}
+}
