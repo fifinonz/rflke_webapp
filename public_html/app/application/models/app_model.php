57bad0c41ad9db34a5b22d3ebcb3e1d5b740033f
Muthoni <muthoni.nguthi@betterworldsystems.co.ke>
2015-07-03 11:12:54 +0300

    created app model for the cart

--- public_html/app/application/models/app_model.php
+++ public_html/app/application/models/app_model.php
@@ -1,5 +1,13 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 class App_model extends CI_Model {
-	private $data;
+    // Function to retrieve an array with all product information
+    function retrieve_products(){
+        $query = $this->db->get('product'); // Select the table product
+        return $query->result_array(); // Return the results in a array.
+    }
+
 }
+
+/* End of file app_model.php */
+/* Location: ./application/models/app_model.php */
