af9aee3787cb0b3d52e9d77fd0ef644785ae7c44
Muthoni <muthoni.nguthi@betterworldsystems.co.ke>
2015-07-23 23:24:58 +0300

    more templates

---
+++ app/application/views/inc/tmp_sec.php
@@ -0,0 +1,13 @@
+<?php
+/************************************************************************/
+//  Protected
+// *********************************************************************
+// Accessed Only By Users Who are logged in!//
+
+    $this->load->view('inc/secure_header');
+    $this->load->view($secure__content);
+    $this->load->view('inc/footer');
+
+
+
+      /* End of file 'tmp_sec.php */
