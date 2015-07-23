b7547f30a6783ed0b5c077e5b20e9b3955f652f8
Muthoni <muthoni.nguthi@betterworldsystems.co.ke>
2015-07-23 23:24:05 +0300

    added email config file

---
+++ app/application/config/email.php
@@ -0,0 +1,13 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+/*
+| -------------------------------------------------------------------
+| EMAIL SENDING SETTINGS
+| -------------------------------------------------------------------
+*/
+
+$config['protocol'] = 'mail';
+$config['wordwrap'] = false;
+$config['mailtype'] = 'html';
+
+/* End of file email.php */
+/* Location: ./application/config/email.php */
