87436983da86ed154b56c062f1261f874b31351c
Muthoni <muthoni.nguthi@betterworldsystems.co.ke>
2015-07-27 15:08:06 +0300

    login/signup toggle

--- app/application/modules/site/views/teams_2015.php
+++ app/application/modules/site/views/teams_2015.php
@@ -1,3 +1,27 @@
+<?php session_start();?>
+<?php
+// SET REFERRER
+
+function strleft($s1, $s2) {
+    return substr($s1, 0, strpos($s1, $s2));
+}
+
+function selfURL() {
+    if(!isset($_SERVER['REQUEST_URI'])) {
+        $serverrequri = $_SERVER['PHP_SELF'];
+    }
+    else {
+        $serverrequri = $_SERVER['REQUEST_URI'];
+    }
+    $s = empty($_SERVER["HTTPS"]) ? '' : ($_SERVER["HTTPS"] == "on") ? "s" : "";
+    $protocol = strleft(strtolower($_SERVER["SERVER_PROTOCOL"]), "/").$s;
+    $port = ($_SERVER["SERVER_PORT"] == "80") ? "" : (":".$_SERVER["SERVER_PORT"]);
+    $_SESSION['ref'] = $protocol."://>/".$_SERVER['SERVER_NAME'].$port.$serverrequri;
+}
+
+selfURL();
+
+?>
 <div id="layerslider-container-fw">
     <div id="layerslider" style="width: 100%; height: 450px; margin: 0px auto; ">
         <div class="ls-slide" data-ls="transition3d:53; timeshift:-1000;">
@@ -9,9 +33,6 @@
     </div>
 </div><!-- Layer Slider -->
 
-<div class="top-image">
-    <img src="<?php echo base_url();?>assets/images/single-page-top2.jpg" alt="" />
-</div><!--Page Top Image-->
 
 <section class="inner-page">
     <div class="container">
@@ -20,6 +41,44 @@
         </div><!--Page Title-->
         <div class="remove-ext">
             <div class="row">
+                <div class="accordion_toggle"><a href="#">
+                        <span>
+                            <i class="icon-info-sign"></i>
+                        </span>To join a team you must be logged in. Click here to Log in.</a>
+                </div>
+                <div class="accordion_content" style="display: block;">
+                    <div class="login">
+                        <p style="font-size: 16px;">Please log in below.</p>
+                        <div class="row">
+                            <div class="col-md-6 co-form half-field">
+                                <label>Username or email <span>*</span></label>
+                                <input type="text" class="form-control input-field">
+                            </div>
+                            <div class="col-md-6 co-form half-field">
+                                <label>Password<span>*</span></label>
+                                <input type="password" class="form-control input-field">
+                            </div>
+                        </div>
+                        <input type="submit" class="cart-btn pull-left" value="Login">
+                    </div>
+                </div>
+                <div class="accordion_toggle"><a href="#">
+                        <span><i class="icon-info-sign"></i>
+                        </span>Don't have a Relay account? Click here to Sign up!</a>
+                </div>
+                <div class="accordion_content" style="display: block;">
+                    <div class="login">
+                        <p style="font-size: 16px;">
+                            Please Create an account with us...it is absolutely free! Having an account will allow you to join a team or start a new team!</p>
+                        <br/>
+                        <div class="row">
+                            <div class="col-md-6 co-form half-field">
+                                <a class="header-btn" href="register.html" title="">SIGN UP NOW</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <section class="block">
                 <div class="col-md-4">
                     <div class="our-cause">
                         <div class="our-cause-img">
@@ -118,7 +177,7 @@
                         </div>
                     </div>
                 </div>
-
+                </section>
             </div>
         </div>
 
@@ -140,5 +199,6 @@
         </div><!--Pagination-->
 
     </div>
+
 </section>
 
