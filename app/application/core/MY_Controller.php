dc43d96b02e6253897853732778320c3df2cb73f
Muthoni <muthoni.nguthi@betterworldsystems.co.ke>
2015-07-27 16:06:02 +0300

    added MY_Controller

---
+++ app/application/core/MY_Controller.php
@@ -0,0 +1,22 @@
+<?php (defined('BASEPATH')) OR exit('No direct script access allowed');
+
+/* load the MX_Loader class */
+require APPPATH."third_party/MX/Controller.php";
+
+
+
+
+class MY_Controller extends MX_Controller
+{
+    protected $data;
+       function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function _load_view(){
+        $this->load->view('inc/tmp',$this->data);
+    }
+
+
+}
