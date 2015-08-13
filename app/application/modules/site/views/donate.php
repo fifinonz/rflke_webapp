fbbb6cd7de1bd7c1f4bac938afc61c94ec96c2b9
Muthoni <muthoni.nguthi@betterworldsystems.co.ke>
2015-08-13 13:13:55 +0300

    added navbar. removed unused links

--- app/application/modules/site/views/donate.php
+++ app/application/modules/site/views/donate.php
@@ -3,12 +3,14 @@
     <img src="<?php echo base_url();?>assets/images/faq-top.jpg" alt="" />
 </div><!-- Page Top Image -->
 
-<section class="inner-page">
+<section class="inner-page switch">
     <div class="container">
+
         <div class="row">
             <div class="left-content col-md-9">
+
                 <div class="single-service">
-                    <span><i class="icon-cogs"></i></span>
+                    <span><i style="color: #800080" class="icon-cogs"></i></span>
                     <h1> How Does The Money Raised Through Relay For Life® Make A Difference?</h1>
                     <p style="font-size: 18px;">Donations raised through Relay For Life KE support the Kenya Cancer Association efforts in rolling out cancer  education programmes in the rural areas to provide up-to-date cancer prevention and early detection information to educate people about the disease, establishing a toll free line for cancer information and cancer activities as well as offering free free programs and services to improve the quality of life for people facing cancer and their families. Visit our webpage
                         <a href="http://www.kenyacancer.org/">Kenya Cancer Association</a>
@@ -19,91 +21,121 @@
                         <h2><strong>How to Make A Donation</strong></h2><br/>
 
                         <ul style="font-size: 18px;">
-                            <li><i class="icon-check"></i> Go to <strong>MPESA Menu</strong></li>
-                            <li><i class="icon-check"></i> Select <strong>Lipa na MPESA</strong></li>
-                            <li><i class="icon-check"></i> Enter Business number <strong>288773</strong></li>
-                            <li><i class="icon-check"></i> Enter the word <strong>DONATION</strong> as the account number</li>
-                            <li><i class="icon-check"></i> Enter Amount to send, your MPESA pin and confirm the transaction</li>
+                            <li><i style="color: #800080" class="icon-check"></i> Go to <strong>MPESA Menu</strong></li>
+                            <li><i style="color: #800080" class="icon-check"></i> Select <strong>Lipa na MPESA</strong></li>
+                            <li><i style="color: #800080" class="icon-check"></i> Enter Business number <strong>288773</strong></li>
+                            <li><i style="color: #800080" class="icon-check"></i> Enter the word <strong>DONATION</strong> as the account number</li>
+                            <li><i style="color: #800080" class="icon-check"></i> Enter Amount to send, your MPESA pin and confirm the transaction</li>
                         </ul>
                     </div>
                 </div>
-                </div>
+
+
+</div>
+            <div class="sidebar col-md-3 pull-right">
+                <div class="sidebar-widget">
+                    <div class="sidebar-title">
+                        <h4>RELAY FOR LIFE<span> 2015</span></h4>
+                    </div>
+                    <ul class="sidebar-list">
+                        <li><a href="<?php echo base_url();?>site/relay/what_is_relay" title="" title="">Learn About Relay</a></li>
+                        <li><a href="<?php echo base_url();?>auth/create_team"" title="">Start a Team</a></li>
+                        <li><a href="<?php echo base_url();?>auth/teams_2015" title="">Join a Team</a></li>
+                        <!--                            <li><a href="#" title="">Nature (7)</a></li>-->
+                        <!--                            <li><a href="#" title="">Scenery(3)</a></li>-->
+                        <!--                            <li><a href="#" title="">Uncategorized(1)</a></li>-->
+                    </ul>
+                </div><!-- Category List -->
+                <div class="sidebar-widget">
+                    <div class="sidebar-title">
+                        <h4>Quick <span>Links</span></h4>
+                    </div>
+                    <div class="cloud-tags">
+                        <a href="<?php echo base_url();?>site/relay/what_is_relay" title="">What is Relay?</a>
+                        <a href="<?php echo base_url();?>site/relay/donate" title="">Donate</a>
+                        <a href="<?php echo base_url();?>site/relay/faqs" title="">FAQs</a>
+                        <a href="<?php echo base_url();?>assets/images/RFL_P.jpg" title="">When is Relay</a>
+                        <a href="#" title="">Find a Team</a>
+                        <a href="<?php echo base_url();?>auth/teams_2015" title="">Join a Team</a>
+                        <a href="<?php echo base_url();?>auth/login" title="">Log In</a>
+                        <!--                            <a href="#" title="">Uncategorized</a>-->
+                        <!--                            <a href="#" title="">Susipit</a>-->
+                    </div>
+                </div><!-- Tags Clouds -->
+                <div class="sidebar-widget">
+                    <div class="sidebar-title">
+                        <h4>Mailing <span>List</span></h4>
+                    </div>
+                    <!-- Begin MailChimp Signup Form -->
+                    <link href="//cdn-images.mailchimp.com/embedcode/classic-081711.css" rel="stylesheet" type="text/css">
+                    <style type="text/css">
+                        #mc_embed_signup{background:#fff; clear:left; font:14px Helvetica,Arial,sans-serif; }
+                        /* Add your own MailChimp form style overrides in your site stylesheet or in this style block.
+                           We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
+                    </style>
+                    <div id="mc_embed_signup">
+                        <form action="//relayforlifekenya.us11.list-manage.com/subscribe/post?u=2f7e5fba8add20ab340278136&amp;id=e3ce9d8083" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+                            <div id="mc_embed_signup_scroll">
+                                <h4>Subscribe to our mailing list</h4>
+                                <div class="indicates-required"><span class="asterisk">*</span> indicates required</div>
+                                <div class="mc-field-group">
+                                    <label for="mce-EMAIL">Email Address  <span class="asterisk">*</span>
+                                    </label>
+                                    <input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
+                                </div>
+                                <div class="mc-field-group">
+                                    <label for="mce-FNAME">First Name </label>
+                                    <input type="text" value="" name="FNAME" class="" id="mce-FNAME">
+                                </div>
+                                <div class="mc-field-group">
+                                    <label for="mce-LNAME">Last Name </label>
+                                    <input type="text" value="" name="LNAME" class="" id="mce-LNAME">
+                                </div>
+                                <div id="mce-responses" class="clear">
+                                    <div class="response" id="mce-error-response" style="display:none"></div>
+                                    <div class="response" id="mce-success-response" style="display:none"></div>
+                                </div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+                                <div style="position: absolute; left: -5000px;"><input type="text" name="b_2f7e5fba8add20ab340278136_e3ce9d8083" tabindex="-1" value=""></div>
+                                <div style="color:#2b0171" class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+                            </div>
+                        </form>
+                    </div>
+
+                    <!--End mc_embed_signup-->
+                </div><!-- Subscribe Widget -->
+                <div class="sidebar-widget">
+                    <div class="sidebar-title">
+                        <h4>Support <span>Us</span></h4>
+                    </div>
+                    <div class="donate-us">
+                        <h3>Give Your Donations</h3>
+                        <span><strong>MPESA PAYBILL: 288773</strong></span>
+                        <!--                            <div class="collected">-->
+                        <!--                                <p>Collected Donations</p>-->
+                        <!--                                <span><strong>$</strong> 7,089,7!</span>-->
+                        <!--                            </div>-->
+                        <div style="margin-top:28px" class="">
+                            <a title="" class=" header-btn" href="<?php echo base_url();?>site/relay/donate">Donate Now</a>
+                        </div>
+                    </div>
+                </div><!--Donate Us -->
+
+                <div class="container">
+
+                    <ul class="social-bar">
+                        <li><a title="" href="#"><img alt="" src="<?php echo base_url();?>assets/images/youtube.png"></a></li>
+                        <li><a title="" href="https://www.facebook.com/Relay4LifeKenya"><img alt="" src="<?php echo base_url();?>assets/images/facebook.png"></a></li>
+                        <li><a title="" href="https://www.twitter.com/Relay4LifeKE"><img alt="" src="<?php echo base_url();?>assets/images/twitter.png"></a></li>
+                        <li><a title="" href="https://instagram.com/relayforlifekenya/"><img alt="" src="<?php echo base_url();?>assets/images/instagram.jpg"></a></li>
+
+                    </ul>
+                    <h4>FIND US ON SOCIAL MEDIA.</h4>
+                </div><!-- Social Media Bar -->
 
             </div>
-
 
         </div>
 
-
+</div>
 </section>
-<section class="block">
-    <div class="container">
-        <div class="donation-bar">
-            <div class="amount pull-left">
-                <p>Our Target For 2015</p>
-                <span><strong>KES</strong> 10,000,000!</span>
-            </div>
-            <div class="donate-now">
-                <a class="donate-button"  title="">THANK YOU!!</a>
-            </div>
-            <div class="amount pull-right">
-                <p>Collected Dontaions</p>
-                <span><strong>KES</strong> 1,089,758!</span>
-            </div>
-
-        </div><!-- Donation Bar -->
-    </div>
-</section>
-
-<section class="block">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12">
-                <div class="sec-heading">
-                    <h2><strong>RELATED </strong>PAGES</h2>
-                </div>
-            </div>
-            <div class="services">
-                <div class="col-md-2">
-                    <div class="box">
-                        <i class="icon-user"></i>
-                        <h4><a style="color:#FFF; font-size: 14px" href="#">Meet The Team</a></h4>
-                    </div>
-                </div>
-                <div class="col-md-2">
-                    <div class="box">
-                        <i class="icon-question-sign"></i>
-                        <h4><a style="color:#FFF; font-size: 14px" href="about_relay">What is Relay for Life?</a></h4>
-                    </div>
-                </div>
-                <div class="col-md-2">
-                    <div class="box">
-                        <i class="icon-microphone"></i>
-                        <h4><a style="color:#FFF; font-size: 14px" href="#">Meet The Shujaa's</a></h4>
-                    </div>
-                </div>
-                <div class="col-md-2">
-                    <div class="box">
-                        <i class="icon-shopping-cart"></i>
-                        <h4><a style="color:#FFF; font-size: 14px" href="#">Buy our Merchandise</a></h4>
-                    </div>
-                </div>
-                <div class="col-md-2">
-                    <div class="box">
-                        <i class="icon-group"></i>
-                        <h4><a style="color:#FFF; font-size: 14px" href="#">Join a Team</a></h4>
-                    </div>
-                </div>
-                <div class="col-md-2">
-                    <div class="box">
-                        <i class="icon-hand-up"></i>
-                        <h4>Volunteer</h4>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</section>
-
-
 
