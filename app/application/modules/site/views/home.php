ec88a487f1a1b48233e88cf4da936aacd577987c
Muthoni <muthoni.nguthi@betterworldsystems.co.ke>
2015-08-13 12:39:49 +0300

    Added navigation sidebar

--- app/application/modules/site/views/home.php
+++ app/application/modules/site/views/home.php
@@ -2,8 +2,12 @@
 <!--    <img src="--><?php //echo base_url();?><!--assets/images/RFL_P.jpg" alt="" />-->
 </div><!-- Page Top Image -->
 
+<section class="inner-page switch">
+    <div class="container">
 
-<section class="block remove-top">
+        <div class="row">
+            <div class="left-content col-md-9">
+                <section class="block remove-top">
     <div class="container">
         <div class="our-cause-sec">
             <div class="sec-title">
@@ -58,14 +62,18 @@
                                 </div>
                             </div><!-- Layer Slider -->
                             <div class="details">
+                                <h1><i style="color:#2b0171">What is Relay For Life?</i></h1>
+                                <br/><p style="font-size: 22px">Relay For Life is an overnight community event that celebrates cancer survivors, remembers those lost and rallies everyone to fight back so we can see a day when all cancers are cured. Anybody can join in, regardless of age or fitness level. Relays are fun for all the family and everyone is welcome!</p>
+
+
+
+                                <br/><p style="font-size: 22px;margin-top: 26px">This year our Relay for Life Event is themed Cancer Education, Awareness and Patient Support. Funds raised will be used to empower communities with information about the preventiton, detection and treatment of Cancer<br/>
+                                  <a class="main-container fixer-container signup-btn" href="<?php echo base_url();?>auth/view_teams">Sign Up For Relay 2015</a>
+                                </p>
                                 <div class="needed-amount">
                                     <h5><i>KSH.</i>40M<span>OUR TARGET</span></h5>
                                 </div>
-
-                                <p style="font-size: 18px">This year our Relay for Life Event is themed Cancer Education, Awareness and Patient Support. Funds raised will be used to empower communities with information about the preventiton, detection and treatment of Cancer<br/>
-                                  <a class="main-container fixer-container signup-btn" href="<?php echo base_url();?>auth/view_teams">Sign Up For Relay 2015</a>
-                                </p>
-                            </div>
+                        </div>
 
                         </div>
                     </div>
@@ -79,73 +87,102 @@
 </section>
 <section class="block">
     <div class="container">
-        <div class="lastevent-stats-container">
-            <div class="lastevent-stats">
-                <!-- RFL_NATL_2015_reus_footer_stats -->
-                <div class="lastevent-stats-col left first">
-                    <div class="lastevent-stats-num">2014</div>
-                    <div class="lastevent-stats-text">1st Relay Event In Kenya</div>
-                </div>
-                <div class="lastevent-stats-col left">
-                    <div class="lastevent-stats-num">1,234</div>
-                    <div class="lastevent-stats-text">Participants</div>
-                </div>
-                <div class="lastevent-stats-col right">
-                    <div class="lastevent-stats-num">Ksh.6.25M</div>
-                    <div class="lastevent-stats-text">Money raised</div>
-                </div>
-
-
-                <!-- end RFL_NATL_2015_reus_footer_stats -->
-                <div class="clearme"></div>
-            </div>
-        </div>    </div>
-</section>
-
-<section class="block">
-    <div class="container">
         <div class="row">
-            <div class="col-md-5">
-                    <div class="sec-title">
-                        <h2><span>NEWSLETTER</span></h2>
-                    </div>
-                <!-- Begin MailChimp Signup Form -->
-                <link href="//cdn-images.mailchimp.com/embedcode/classic-081711.css" rel="stylesheet" type="text/css">
-                <style type="text/css">
-                    #mc_embed_signup{background:#fff; clear:left; font:14px Helvetica,Arial,sans-serif; }
-                    /* Add your own MailChimp form style overrides in your site stylesheet or in this style block.
-                       We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
-                </style>
-                <div id="mc_embed_signup">
-                    <form action="//relayforlifekenya.us11.list-manage.com/subscribe/post?u=2f7e5fba8add20ab340278136&amp;id=e3ce9d8083" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-                        <div id="mc_embed_signup_scroll">
-                            <h2>Subscribe to our mailing list</h2>
-                            <div class="indicates-required"><span class="asterisk">*</span> indicates required</div>
-                            <div class="mc-field-group">
-                                <label for="mce-EMAIL">Email Address  <span class="asterisk">*</span>
-                                </label>
-                                <input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
-                            </div>
-                            <div class="mc-field-group">
-                                <label for="mce-FNAME">First Name </label>
-                                <input type="text" value="" name="FNAME" class="" id="mce-FNAME">
-                            </div>
-                            <div class="mc-field-group">
-                                <label for="mce-LNAME">Last Name </label>
-                                <input type="text" value="" name="LNAME" class="" id="mce-LNAME">
-                            </div>
-                            <div id="mce-responses" class="clear">
-                                <div class="response" id="mce-error-response" style="display:none"></div>
-                                <div class="response" id="mce-success-response" style="display:none"></div>
-                            </div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-                            <div style="position: absolute; left: -5000px;"><input type="text" name="b_2f7e5fba8add20ab340278136_e3ce9d8083" tabindex="-1" value=""></div>
-                            <div style="color:#2b0171" class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
-                        </div>
-                    </form>
-                </div>
+            <iframe width="100%" height="400" src="https://www.youtube.com/embed/h_9mtpibVfo" frameborder="0" allowfullscreen></iframe>
 
-                <!--End mc_embed_signup-->
-                <br/>
+        </div>
+    </div>
+
+</section>
+                </div>
+            <div class="sidebar col-md-3 pull-right">
+                <div class="sidebar-widget">
+                    <div class="sidebar-title">
+                        <h4>RELAY FOR LIFE<span> 2015</span></h4>
+                    </div>
+                    <ul class="sidebar-list">
+                        <li><a href="<?php echo base_url();?>site/relay/what_is_relay" title="" title="">Learn About Relay</a></li>
+                        <li><a href="<?php echo base_url();?>auth/create_team"" title="">Start a Team</a></li>
+                        <li><a href="<?php echo base_url();?>auth/teams_2015" title="">Join a Team</a></li>
+                        <!--                            <li><a href="#" title="">Nature (7)</a></li>-->
+                        <!--                            <li><a href="#" title="">Scenery(3)</a></li>-->
+                        <!--                            <li><a href="#" title="">Uncategorized(1)</a></li>-->
+                    </ul>
+                </div><!-- Category List -->
+                <div class="sidebar-widget">
+                    <div class="sidebar-title">
+                        <h4>Quick <span>Links</span></h4>
+                    </div>
+                    <div class="cloud-tags">
+                        <a href="<?php echo base_url();?>site/relay/what_is_relay" title="">What is Relay?</a>
+                        <a href="<?php echo base_url();?>site/relay/donate" title="">Donate</a>
+                        <a href="<?php echo base_url();?>site/relay/faqs" title="">FAQs</a>
+                        <a href="<?php echo base_url();?>assets/images/RFL_P.jpg" title="">When is Relay</a>
+                        <a href="#" title="">Find a Team</a>
+                        <a href="<?php echo base_url();?>auth/teams_2015" title="">Join a Team</a>
+                        <a href="<?php echo base_url();?>auth/login" title="">Log In</a>
+                        <!--                            <a href="#" title="">Uncategorized</a>-->
+                        <!--                            <a href="#" title="">Susipit</a>-->
+                    </div>
+                </div><!-- Tags Clouds -->
+                <div class="sidebar-widget">
+                    <div class="sidebar-title">
+                        <h4>Mailing <span>List</span></h4>
+                    </div>
+                    <!-- Begin MailChimp Signup Form -->
+                    <link href="//cdn-images.mailchimp.com/embedcode/classic-081711.css" rel="stylesheet" type="text/css">
+                    <style type="text/css">
+                        #mc_embed_signup{background:#fff; clear:left; font:14px Helvetica,Arial,sans-serif; }
+                        /* Add your own MailChimp form style overrides in your site stylesheet or in this style block.
+                           We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
+                    </style>
+                    <div id="mc_embed_signup">
+                        <form action="//relayforlifekenya.us11.list-manage.com/subscribe/post?u=2f7e5fba8add20ab340278136&amp;id=e3ce9d8083" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+                            <div id="mc_embed_signup_scroll">
+                                <h4>Subscribe to our mailing list</h4>
+                                <div class="indicates-required"><span class="asterisk">*</span> indicates required</div>
+                                <div class="mc-field-group">
+                                    <label for="mce-EMAIL">Email Address  <span class="asterisk">*</span>
+                                    </label>
+                                    <input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
+                                </div>
+                                <div class="mc-field-group">
+                                    <label for="mce-FNAME">First Name </label>
+                                    <input type="text" value="" name="FNAME" class="" id="mce-FNAME">
+                                </div>
+                                <div class="mc-field-group">
+                                    <label for="mce-LNAME">Last Name </label>
+                                    <input type="text" value="" name="LNAME" class="" id="mce-LNAME">
+                                </div>
+                                <div id="mce-responses" class="clear">
+                                    <div class="response" id="mce-error-response" style="display:none"></div>
+                                    <div class="response" id="mce-success-response" style="display:none"></div>
+                                </div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+                                <div style="position: absolute; left: -5000px;"><input type="text" name="b_2f7e5fba8add20ab340278136_e3ce9d8083" tabindex="-1" value=""></div>
+                                <div style="color:#2b0171" class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+                            </div>
+                        </form>
+                    </div>
+
+                    <!--End mc_embed_signup-->
+                </div><!-- Subscribe Widget -->
+                <div class="sidebar-widget">
+                    <div class="sidebar-title">
+                        <h4>Support <span>Us</span></h4>
+                    </div>
+                    <div class="donate-us">
+                        <h3>Give Your Donations</h3>
+                        <span><strong>MPESA PAYBILL: 288773</strong></span>
+                        <!--                            <div class="collected">-->
+                        <!--                                <p>Collected Donations</p>-->
+                        <!--                                <span><strong>$</strong> 7,089,7!</span>-->
+                        <!--                            </div>-->
+                        <div style="margin-top:28px" class="">
+                            <a title="" class=" header-btn" href="<?php echo base_url();?>site/relay/donate">Donate Now</a>
+                        </div>
+                    </div>
+                </div><!--Donate Us -->
+
                 <div class="container">
 
                     <ul class="social-bar">
@@ -155,35 +192,37 @@
                         <li><a title="" href="https://instagram.com/relayforlifekenya/"><img alt="" src="<?php echo base_url();?>assets/images/instagram.jpg"></a></li>
 
                     </ul>
-                    <h3>FIND US ON SOCIAL MEDIA.</h3>
+                    <h4>FIND US ON SOCIAL MEDIA.</h4>
                 </div><!-- Social Media Bar -->
 
-
-                        </div>	<!-- News Letter SignUp --> <!-- Newsletter -->
-
-            <div class="col-md-6 pull-right">
-                <div class="sec-title">
-                    <h2><span>FEATURE VIDEO</span></h2>
-                </div>
-                <div class="charity-video">
-                    <div class="row">
-                        <div class="col-md-6">
-                            <div class="">
-                                <!--                                <img src="--><?php //echo base_url();?><!--assets/images/video-bg.png" alt="" />-->
-                                <!--                                <a title="A call to Relay" href="https://www.youtube.com/watch?v=mL-z5h_mJBE" class="html5lightbox">-->
-                                <iframe width="300" height="300" src="https://www.youtube.com/embed/h_9mtpibVfo" frameborder="0" allowfullscreen></iframe>
-
-                            </div>
-                        </div>
-                        <div class="col-md-6 desc">
-                            <h3><span>A CALL TO RELAY</span></h3>
-                            <p style="font-size: 16px">Together we will make sure cancer in the country is accorded the attention it deserves.</p>
-                        </div>
+            </div>
+        </div>
+        </div>
+    <section class="block">
+        <div class="container">
+            <div class="lastevent-stats-container">
+                <div class="lastevent-stats">
+                    <!-- RFL_NATL_2015_reus_footer_stats -->
+                    <div class="lastevent-stats-col left first">
+                        <div class="lastevent-stats-num">2014</div>
+                        <div class="lastevent-stats-text">1st Relay Event In Kenya</div>
+                    </div>
+                    <div class="lastevent-stats-col left">
+                        <div class="lastevent-stats-num">1,234</div>
+                        <div class="lastevent-stats-text">Participants</div>
+                    </div>
+                    <div class="lastevent-stats-col right">
+                        <div class="lastevent-stats-num">Ksh.6.25M</div>
+                        <div class="lastevent-stats-text">Money raised</div>
                     </div>
 
+
+                    <!-- end RFL_NATL_2015_reus_footer_stats -->
+                    <div class="clearme"></div>
                 </div>
-            </div><!-- Charity Video -->
-        </div>
-    </div>
+            </div>    </div>
+    </section>
+
 </section>
+
 
